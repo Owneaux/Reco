345b2ca04da8aeb45075c4db88987e93efb7e146
miguel.maquieira <miguel.maquieira@gmail.com>
2015-02-28 16:21:02 +0100

    deals page navigating

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,4 @@
 class Business < ActiveRecord::Base
   has_many :deal_types
+  has_many :deals through: deal_types
 end
--- app/models/deal_type.rb
+++ app/models/deal_type.rb
@@ -1,3 +1,4 @@
 class DealType < ActiveRecord::Base
   belongs_to: business
+  has_many: deals
 end
