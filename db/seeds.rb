1150d327017a35ef31c9cc9e425b83be616172be
Onno Schoemaker <oschoemaker@gmail.com>
2015-03-12 17:58:22 +0100

    Icon, home screen links and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def rand_in_range(from, to)
-  rand * (to - from) + from
+
+
+
+30.times do
+  promoter = Promoter.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone,
+    password: '11111111'
+  )
+  promoter.save
 end
-
-def rand_time(from, to=Time.now)
-  Time.at(rand_in_range(from.to_f, to.to_f))
-end
-
-
-
-# 30.times do
-#   promoter = Promoter.create(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     phone: Faker::PhoneNumber.cell_phone,
-#     password: '11111111'
-#   )
-#   promoter.save
-# end
-
-# 5.times do |i|
-#   deal = Deal.create(
-#     deal_type: 4,
-#     promoter_id: 3,
-#     business_id: 1,
-#     referrals: rand(20..40),
-#     created_at: rand((i + 3).day).ago
-#     )
-#   deal.save
-# end
 
